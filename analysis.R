631e9524a461b18bb268fea46e2f764cb8fd897f
bahniks <bahniks@seznam.cz>
2015-09-15 00:26:24 +0200

    almost final

--- analysis.R
+++ analysis.R
@@ -62,55 +62,64 @@ d <- read.csv("edge1.1.csv", head = T, as.is = T)
 remove <- d$UniqueContributors < 2 & d$Type == 2
 d <- d[!remove,]
 d$Role <- factor(d$Role)
+d <- cbind(d, ExtremeStatus = abs(3.5 - d$AcademicHierarchyStrict))
 
 mccall <- function(x) qnorm((rank(x, ties.method = "average") - 0.5) / length(x))
+
 qd <- d[d$Type == 1,]
-cd <- d[d$Type == 2 & d$Role == 2,]
-qd <- cbind(qd, mccall.WC = mccall(qd$WC))
+qd <- cbind(qd, verbosity = mccall(qd$WC))
+
+cd <- d[d$Type == 2 & d$WC > 50,]
+cd <- cbind(cd, dominance = mccall(mccall(cd$we) - mccall(cd$i)))
+cd <- cbind(cd, verbosity = mccall(cd$WC))
+cd <- cbind(cd, femaleproportion = cd$UniqueFemaleContributors/cd$UniqueContributors)
+
+dd <- d[sapply(unique(d$Title), function(x) which(d$Title == x)[1]),]
+dd <- dd[dd$Type == 2 & dd$UniqueMaleContributors > 0 & dd$UniqueFemaleContributors > 0,]
+dd <- cbind(dd, femaleproportion = dd$UniqueFemaleContributors/dd$UniqueContributors)
+dd <- cbind(dd, overrepresented = (dd$FemaleParticipation > dd$femaleproportion))
 
 
 # Hypothesis 1a
-m1a.Q <- lmer(mccall.WC ~ AcademicHierarchyStrict + + (1|ThreadId) + (1|Id), 
-              data = d[d$Type == 1,])
-summary(m1a.Q)
-m1a.C <- lmer(Number.Characters ~ AcademicHierarchyStrict + UniqueContributors + (1|Id), 
-              data = d[d$Type == 2 & d$Role == 2,])
-summary(m1a.C)
+m1a <- lmer(verbosity ~ AcademicHierarchyStrict + (1|ThreadId) + (1|Id), data = qd)
+summary(m1a)
   
 # Hypothesis 1b
-m1b.iQ <- lmer(i ~ AcademicHierarchyStrict + (1|Id), data = d[d$Type == 1,])
-summary(m1b.iQ)
-m1b.weQ <- lmer(we ~ AcademicHierarchyStrict + (1|Id), data = d[d$Type == 1,])
-summary(m1b.weQ)
-
-m1b.iC <- lmer(i ~ AcademicHierarchyStrict*Role + (1|ThreadId) + (1|Id), 
-               data = d[d$Type == 2 & d$WC > 10,])
-summary(m1b.iC)
-m1b.weC <- lmer(we ~ AcademicHierarchyStrict*Role + (1|ThreadId) + (1|Id), 
-                data = d[d$Type == 2 & d$WC > 10,])
-summary(m1b.weC)
-
+m1b <- lmer(dominance ~ AcademicHierarchyStrict + Role + (1|ThreadId) + (1|Id), data = cd)
+summary(m1b)
 
 # Hypothesis 1c
-m1c.Q <- lmer(Number.Characters ~ Female + (1|Id), data = d[d$Type == 1,])
-summary(m1c.Q)
-m1c.C <- lmer(Number.Characters ~ Female + UniqueContributors + (1|Id), 
-              data = d[d$Type == 2 & d$Role == 2,])
-summary(m1c.C)
+m1c <- lmer(verbosity ~ Male + (1|ThreadId) + (1|Id), data = qd)
+summary(m1c)
 
 # Hypothesis 1d
-m1b.iQ <- lmer(i ~ Female + (1|Id), data = d[d$Type == 1,])
-summary(m1b.iQ)
-m1b.weQ <- lmer(we ~ Female + (1|Id), data = d[d$Type == 1,])
-summary(m1b.weQ)
+m1d <- lmer(dominance ~ Male + Role + (1|ThreadId) + (1|Id), data = cd)
+summary(m1d)
 
-m1b.iC <- lmer(i ~ Female*Role + (1|ThreadId) + (1|Id), 
-               data = d[d$Type == 2 & d$WC > 10,])
-summary(m1b.iC)
-m1b.weC <- lmer(we ~ Female*Role + (1|ThreadId) + (1|Id), 
-                data = d[d$Type == 2 & d$WC > 10,])
-summary(m1b.weC)
+# Hypothesis 2a
+m2a <- lmer(verbosity ~ Male*AcademicHierarchyStrict + (1|ThreadId) + (1|Id), data = qd)
+summary(m2a)
 
+# Hypothesis 2b
+m2b <- lmer(dominance ~ Male*AcademicHierarchyStrict + Role + (1|ThreadId) + (1|Id), data = cd)
+summary(m2b)
 
+# Hypothesis 3a
+m3a <- lmer(verbosity ~ ExtremeStatus + (1|ThreadId) + (1|Id), data = qd)
+summary(m3a)
 
+# Hypothesis 3b
+m3b <- lmer(dominance ~ ExtremeStatus + Role + (1|ThreadId) + (1|Id), data = cd)
+summary(m3b)
 
+# Hypothesis 4
+m4 <- lmer(verbosity ~ Male*femaleproportion + (1|ThreadId) + (1|Id), data = cd)
+summary(m4)
+
+# Hypothesis 5a
+m5a <- lmer(verbosity ~ Male*Live + (1|ThreadId) + (1|Id), data = cd)
+summary(m5a)
+
+# Hypothesis 5b
+m5b <- lmer(dominance ~ Male*Live + Role + (1|ThreadId) + (1|Id), data = cd)
+summary(m5b)
