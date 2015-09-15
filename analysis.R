1c8e5d8bc38870f5569bcf6cecebf5a19b3d921b
bahniks <bahniks@seznam.cz>
2015-09-15 22:45:58 +0200

    final report

--- analysis.R
+++ analysis.R
@@ -10,48 +10,19 @@ for(pack in c("lme4", "lmerTest")) {
 
 
 #===============================================================================
-# HELPER FUNCTIONS
+# HELPER FUNCTION
 #===============================================================================
-# formating p-values
-round_p <- function(x) {
-  if(x < .001) return("p < .001") 
-  if(round(x, 2) == 1) return("p = 1")
-  
-  if(round(x, 3) >= .01) {
-    p <- sprintf("%.2f", x)
-  } else {
-    p <- sprintf("%.3f", x)
-  }
-  
-  return(paste("p = ", substr(p, 2, nchar(p)), sep = ""))
-}
-
 # function for displaying results
-results <- function(model, effect, family = "linear") {
-  coefs <- summary(model)$coefficients
-  if(family == "binomial") {
-    z <- coefs[effect, "z value"]
-    p <- coefs[effect, "Pr(>|z|)"]
-    e.name <- "OR"
-    par <- "z"
-  } else {
-    z <- coefs[effect, "t value"]
-    p <- coefs[effect, "Pr(>|t|)"]        
-    e.name <- "b"
-    par <- paste0("t(", sprintf("%.1f", coefs[effect, "df"]), ")")
-  }
+results <- function(model, effect) {
+  coefs <- summary(model)$coefficients      
+
   ES <- fixef(model)[effect]
   CI <- confint.merMod(model, effect, method = "Wald")
-  if(family == "binomial") {
-    ES <- exp(ES)
-    CI <- exp(CI)
-  }
-  cat(par, " = ", sprintf("%.2f", z), ", ", 
-      round_p(p), ", ",
-      e.name, " = ", sprintf("%.2f", ES), ", ",
-      "95% CI = [", sprintf("%.2f", CI[1]), ", ",
-      sprintf("%.2f", CI[2]), "]",
-      sep = "")
+
+  c(sprintf("%.2f", ES), 
+    paste0("[", sprintf("%.2f", CI[1]), ", ",
+           sprintf("%.2f", CI[2]), "]")
+    )
 }
 
 
@@ -73,11 +44,6 @@ cd <- d[d$Type == 2 & d$WC > 50,]
 cd <- cbind(cd, dominance = mccall(mccall(cd$we) - mccall(cd$i)))
 cd <- cbind(cd, verbosity = mccall(cd$WC))
 cd <- cbind(cd, femaleproportion = cd$UniqueFemaleContributors/cd$UniqueContributors)
-
-dd <- d[sapply(unique(d$Title), function(x) which(d$Title == x)[1]),]
-dd <- dd[dd$Type == 2 & dd$UniqueMaleContributors > 0 & dd$UniqueFemaleContributors > 0,]
-dd <- cbind(dd, femaleproportion = dd$UniqueFemaleContributors/dd$UniqueContributors)
-dd <- cbind(dd, overrepresented = (dd$FemaleParticipation > dd$femaleproportion))
 
 
 # Hypothesis 1a
@@ -113,7 +79,7 @@ m3b <- lmer(dominance ~ ExtremeStatus + Role + (1|ThreadId) + (1|Id), data = cd)
 summary(m3b)
 
 # Hypothesis 4
-m4 <- lmer(verbosity ~ Male*femaleproportion + (1|ThreadId) + (1|Id), data = cd)
+m4 <- lmer(verbosity ~ Female*femaleproportion + (1|ThreadId) + (1|Id), data = cd)
 summary(m4)
 
 # Hypothesis 5a
